6a5efad9c6804d7cfd342366da2ff6ee71e13989
jkea1 <jinkyng7890@gmail.com>
2022-09-20 20:29:09 +0900

    한칸씩 이동하는 슬라이더

---
+++ onecardpassslider/src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+/* import "./styles.css"; */
+
+export default function App() {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 5
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <div style={{ position: "relative" }}>
+        <Carousel responsive={responsive}>
+          <div>Item 1</div>
+          <div>Item 2</div>
+          <div>Item 3</div>
+          <div>Item 4</div>
+          <div>Item 5</div>
+          <div>Item 6</div>
+          <div>Item 7</div>
+          <div>Item 8</div>
+          <div>Item 9</div>
+        </Carousel>
+      </div>
+    </div>
+  );
+}
+
